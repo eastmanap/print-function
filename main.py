52bdb0d83e3cb942b519f36234e27f89e415636f
eastmanap <eastmanap354@northwested.org>
2024-09-09 10:25:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# Apollos Eastman
+# 09 SEP 2024
+# Printing in Python
+
+# Exercise 2 - 3  (Personal Message)
+
+name = 'lebron James'
+
+print (f"Hello {name}, I am a big fan!")
+
+#  Exercise 2 - 5 (Famous Quote)
+
+quote = '"Everybody has a plan until they get punched in the face"'
+
+author = 'Mike Tyson'
+
+print (f"{quote} -{author}")
+
+# Exercise 2 - 6 (Famous Quote 2)
+
+quote = '"Everybody has a plan until they get punched in the face"'
+
+author = 'Mike Tyson'
+
+message = f"{quote} -{author}"
+
+print (f"{message}")
